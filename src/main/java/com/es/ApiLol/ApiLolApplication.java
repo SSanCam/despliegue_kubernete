7a27a565c5d5cd925651bd6b3b2e6cd8afc50026
clase <mgarmorn@gmail.com>
2025-02-13 19:12:01 +0100

    configuracion

--- src/main/java/com/es/ApiLol/ApiLolApplication.java
+++ src/main/java/com/es/ApiLol/ApiLolApplication.java
@@ -4,13 +4,19 @@ import com.es.ApiLol.security.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 @EnableConfigurationProperties(RsaKeyProperties.class)
-public class ApiLolApplication {
+public class ApiLolApplication extends SpringBootServletInitializer { // Extiende SpringBootServletInitializer
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiLolApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApiLolApplication.class);
+	}
 }
